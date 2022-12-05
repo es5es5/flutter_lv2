0bed05113d7df432b0922efe040a66b1d513bf42
es5es5 <es5e5s@nate.com>
2022-12-05 17:09:03 +0900

    login Http

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,14 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_lv2/common/component/custom_text_form_field.dart';
 import 'package:flutter_lv2/common/const/colors.dart';
 import 'package:flutter_lv2/common/layout/dafault_layout.dart';
+import 'package:flutter_lv2/common/view/root_tab.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
+
+  @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+
+    final emulatorIp = '10.0.2.2';
+    final simulatorIp = '127.0.0.1';
+    final port = '3040';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
         child: SingleChildScrollView(
       keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
@@ -29,17 +50,38 @@ class LoginScreen extends StatelessWidget {
               CustomTextFormField(
                 hintText: '이메일을 입력해주세요.',
                 errorText: 'Error!',
-                onChanged: (String value) {},
+                onChanged: (String value) {
+                  username = value;
+                },
               ),
               const SizedBox(height: 16.0),
               CustomTextFormField(
                 hintText: '비밀번호를 입력해주세요.',
                 errorText: 'Error!',
                 obscureText: true,
-                onChanged: (String value) {},
+                onChanged: (String value) {
+                  password = value;
+                },
               ),
               ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    final rawString = '$username:$password';
+
+                    print(rawString);
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                    String token = stringToBase64.encode(rawString);
+                    final response = await dio.post(
+                        'http://$ip:$port/auth/login',
+                        options: Options(
+                            headers: {'authorization': 'Basic $token'}));
+
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => RootTab(),
+                      ),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(primary: PRIMARY_COLOR),
                   child: const Text('로그인')),
               TextButton(
