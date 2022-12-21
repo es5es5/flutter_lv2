dfb889a02bdc23ab6b31d990cdad4d73229acd69
es5es5 <es5e5s@nate.com>
2022-12-21 14:04:00 +0900

    RestaurantRepositoryProvider

--- lib/restaurant/repository/restaurant_repository.dart
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -1,10 +1,23 @@
+import 'package:flutter_lv2/common/const/data.dart';
+import 'package:flutter_lv2/common/dio/dio.dart';
 import 'package:flutter_lv2/common/model/cursor_pagination_model.dart';
 import 'package:flutter_lv2/restaurant/model/restaurant_detail_model.dart';
 import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_lv2/restaurant/model/restaurant_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'restaurant_repository.g.dart';
+
+final restaurantRepositoryProvider = Provider(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+    final repository =
+        RestaurantRepository(dio, baseUrl: '$API_URL/restaurant');
+
+    return repository;
+  },
+);
 
 @RestApi()
 abstract class RestaurantRepository {
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lv2/common/const/data.dart';
 import 'package:flutter_lv2/common/dio/dio.dart';
+import 'package:flutter_lv2/common/model/cursor_pagination_model.dart';
 import 'package:flutter_lv2/restaurant/components/restaurant_card.dart';
 import 'package:flutter_lv2/restaurant/model/restaurant_model.dart';
 import 'package:flutter_lv2/restaurant/repository/restaurant_repository.dart';
@@ -11,33 +12,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class RestaurantScreen extends ConsumerWidget {
   const RestaurantScreen({super.key});
 
-  Future<List<RestaurantModel>> pageRestaurant(WidgetRef ref) async {
-    final dio = ref.watch(dioProvider);
-
-    final response =
-        await RestaurantRepository(dio, baseUrl: '$API_URL/restaurant')
-            .paginate();
-
-    return response.data;
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       child: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: FutureBuilder<List<RestaurantModel>>(
-            future: pageRestaurant(ref),
-            builder: (context, AsyncSnapshot<List<RestaurantModel>> snapshot) {
+          child: FutureBuilder<CursorPagination<RestaurantModel>>(
+            future: ref.watch(restaurantRepositoryProvider).paginate(),
+            builder: (context,
+                AsyncSnapshot<CursorPagination<RestaurantModel>> snapshot) {
               if (!snapshot.hasData) {
                 return const Center(child: CircularProgressIndicator());
               }
 
               return ListView.separated(
-                itemCount: snapshot.data!.length,
+                itemCount: snapshot.data!.data.length,
                 itemBuilder: (_, index) {
-                  final parsedItem = snapshot.data![index];
+                  final parsedItem = snapshot.data!.data[index];
 
                   return GestureDetector(
                     onTap: () {
